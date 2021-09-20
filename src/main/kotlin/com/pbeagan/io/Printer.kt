77172c1b5d5cf6e720211c73db017ae8a997e1de
Patrick Beagan <pbeagan1@gmail.com>
2021-09-20 17:17:18 -0500

    adding in colors and shapes

--- src/main/kotlin/com/pbeagan/io/Printer.kt
+++ src/main/kotlin/com/pbeagan/io/Printer.kt
@@ -1,6 +1,8 @@
 package com.pbeagan.io
 
 import com.pbeagan.entities.IPerson
+import com.pbeagan.entities.IPerson.Female
+import com.pbeagan.entities.IPerson.Male
 import com.pbeagan.entities.Union
 
 interface IPrinter {
@@ -20,23 +22,45 @@ class Printer : IPrinter {
         val out = StringBuilder()
         out.append("digraph {")
         val connections = mutableSetOf<String>()
-        val onUnionFound: (IPerson, Union) -> Unit = { person, union ->
-            connections.add("${union.parent1.dotName()} -> ${union.dotName()}")
-            connections.add("${union.parent2.dotName()} -> ${union.dotName()}")
-            connections.add("${union.dotName()} -> ${person.dotName()}")
+        val onUnionFound: (IPerson, Union) -> Unit = { localPerson, union ->
+            connections.registerStyles(union)
+            connections.addParents(union)
+            connections.addChild(union, localPerson)
         }
         person.searchAncestors(depth, onUnionFound)
         person.searchDescendants(depth) { union ->
             union.parent1.searchAncestors(depth, onUnionFound)
             union.parent2.searchAncestors(depth, onUnionFound)
-            connections.add("${union.parent1.dotName()} -> ${union.dotName()}")
-            connections.add("${union.parent2.dotName()} -> ${union.dotName()}")
+            connections.registerStyles(union)
+            connections.addParents(union)
             union.children.forEach {
-                connections.add("${union.dotName()} -> ${it.dotName()}")
+                connections.addChild(union, it)
             }
         }
         connections.forEach { out.append(it) }
         out.append("}")
         return out.toString()
+    }
+
+    private fun MutableSet<String>.registerStyles(union: Union) {
+        add("${union.dotName()} [shape=\"rectangle\" color=\"green\"]")
+        (union.children + union.parent1 + union.parent2).forEach { person ->
+            when (person) {
+                is Male -> add("${person.dotName()} [shape=\"ellipse\" color=\"blue\"]")
+                is Female -> add("${person.dotName()} [shape=\"ellipse\" color=\"red\"]")
+            }
+        }
+    }
+
+    private fun MutableSet<String>.addChild(
+        union: Union,
+        person: IPerson,
+    ) {
+        add("${union.dotName()} -> ${person.dotName()}")
+    }
+
+    private fun MutableSet<String>.addParents(union: Union) {
+        add("${union.parent1.dotName()} -> ${union.dotName()}")
+        add("${union.parent2.dotName()} -> ${union.dotName()}")
     }
 }
